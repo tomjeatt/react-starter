11251d129f334b54c3c6e1232c01fc5e5dec7dd4
Thomas Jeatt <hello@tomjeatt.com>
2020-11-20 11:57:13 +0000

    Test lint hook

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Container } from './App.style';
+import React from 'react'
+import { Container } from './App.style'
 
 const App: React.FC = () => {
   return (
     <Container>
       <h1>Parcel, React, TypesScript, Styled Components</h1>
     </Container>
-  );
-};
+  )
+}
 
-export default App;
+export default App
