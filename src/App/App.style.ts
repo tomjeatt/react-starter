a4feaf8cf9b24330fc1ca23cbf01423499397875
Thomas Jeatt <hello@tomjeatt.com>
2020-11-20 11:23:26 +0000

    Initial commit

---
+++ src/App/App.style.ts
@@ -0,0 +1,8 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div(() => {
+  return css`
+    background-color: grey;
+    margin: 40px auto;
+  `;
+});
---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './App.style';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <h1>Parcel, React, TypesScript, Styled Components</h1>
+    </Container>
+  );
+};
+
+export default App;
