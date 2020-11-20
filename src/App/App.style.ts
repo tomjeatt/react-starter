0c1ce37c47993f860cba684b59917401d079aa91
tomjeatt <hello@tomjeatt.com>
2020-11-20 13:26:52 +0000

    Tweak container styling

--- src/App/App.style.ts
+++ src/App/App.style.ts
@@ -1,8 +1,8 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components'
 
 export const Container = styled.div(() => {
   return css`
     background-color: grey;
-    margin: 40px auto;
-  `;
-});
+    margin: 50px auto;
+  `
+})
